2361ffc7f914c9c3722ab9a50fad999f5cef21ca
harry.dB <harry.dudleybestow@gmail.com>
2023-01-28 17:20:17 -0800

    first version working and printing. Add base file.

--- print_info.py
+++ print_info.py
@@ -2,6 +2,10 @@ import math
 
 import numpy as np
 import matplotlib.pyplot as plt
+# See here for how to install on windows:
+# https://stackoverflow.com/questions/73637315/oserror-no-library-called-cairo-2-was-found-from-custom-widgets-import-proje
+import cairosvg
+import imageio
 
 '''
 Documentation for the file format comes from:
@@ -9,9 +13,9 @@ https://github.com/cbiffle/catibo/blob/master/doc/cbddlp-ctb.adoc#the-layer-tabl
 See there for all the offsets etc.
 '''
 def decode_image(encoded: np.ndarray, dims: np.ndarray):
+    dims = np.flipud(dims)
     assert(encoded.dtype == np.uint8)
     assert(dims.size == 2)
-    dims = np.flipud(dims)
     values = ((encoded & (1 << 7)) > 0).astype(np.int8)
     lengths = (encoded & 0b01111111).astype('int')
     ptr = 0
@@ -45,29 +49,129 @@ def encode_image(decoded: np.ndarray):
     encoded = np.array(encoded, dtype=np.uint8)
     return encoded
 
+# Again, see github link for magic numbers
+def parse_cbddlp(buf: np.ndarray):
+    layer_table_offset = buf[0x40:0x40+4].view(np.uint32)[0]
 
-fname = r"C:\Users\harry\Documents\Git\catibo\_single_layer_circle.cbddlp"
+    # layer_table = buf[layer_table_offset:layer_table_offset + 0x24]
+    # image_offset = layer_table[0xC:0xC+0x4].view(np.uint32)[0]
+    # image_len = layer_table[0x10:0x10+0x4].view(np.uint32)[0]
+    # # The below dict is a mutable reference to the underlying buffer:
+    # out = {
+    #     'printer_dims':   buf[8:8+3*4].view(np.float32),
+    #     'screen_res':   buf[0x34:0x34+2*4].view(np.uint32),
+    #     'layer_table_offset':   buf[0x40:0x40+4].view(np.uint32),
+    #     'layer_table':   buf[layer_table_offset:layer_table_offset + 0x24],
+    #     'image_offset':   layer_table[0xC:0xC+0x4].view(np.uint32),
+    #     'image_len':   layer_table[0x10:0x10+0x4].view(np.uint32),
+    #     'z_height': layer_table[0x0:0x0+0x4].view(np.float32),
+    #     'exposure_time_s': layer_table[0x4:0x4+0x4].view(np.float32),
+    #     'encoded_image':   buf[image_offset:image_offset + image_len],
+    # }
 
-with open(fname, 'rb') as f:
-    buf = f.read()
-buf = np.frombuffer(buf, dtype=np.uint8)
+    layer_table = buf[layer_table_offset:layer_table_offset + 0x24]
+    image_offset = layer_table[0xC:0xC+0x4].view(np.uint32)[0]
+    image_len = layer_table[0x10:0x10+0x4].view(np.uint32)[0]
+    # The below dict is a mutable reference to the underlying buffer:
+    out = {
+        'printer_dims':   buf[8:8+3*4].view(np.float32),
+        'screen_res':   buf[0x34:0x34+2*4].view(np.uint32),
+        'layer_table_offset':   buf[0x40:0x40+4].view(np.uint32),
+        'layer_table':   buf[layer_table_offset:layer_table_offset + 0x24],
+        'image_offset':   buf[layer_table_offset + 0xC:layer_table_offset+0xC+0x4].view(np.uint32),
+        'image_len':   buf[layer_table_offset + 0x10: layer_table_offset + 0x10+0x4].view(np.uint32),
+        'z_height': buf[layer_table_offset + 0x0: layer_table_offset + 0x0+0x4].view(np.float32),
+        'exposure_time_s': buf[layer_table_offset + 0x4: layer_table_offset + 0x4+0x4].view(np.float32),
+        'encoded_image':   buf[image_offset:image_offset + image_len],
+    }
 
-printer_dims = buf[8:8+3*4].view(np.float32)
-screen_res = buf[0x34:0x34+2*4].view(np.uint32)
-print(f"{printer_dims=}, {screen_res=}")
-layer_table_offset = buf[0x40:0x40+4].view(np.uint32)[0]
-print(f"layer offset: {hex(layer_table_offset)}, {layer_table_offset}")
-layer_table = buf[layer_table_offset:layer_table_offset + 0x24]
-print(layer_table)
-exposure = layer_table[0x4:0x4+0x4].view(np.float32)
-image_offset = layer_table[0xC:0xC+0x4].view(np.uint32)[0]
-image_len = layer_table[0x10:0x10+0x4].view(np.uint32)[0]
-encoded_image = buf[image_offset:image_offset + image_len]
-print(encoded_image)
+    #  make sure it's actually mutable
+    for _, v in out.items(): v.flags.writeable = True
+    return out
 
-img = decode_image(encoded_image, screen_res)
-encoded_diy = encode_image(img)
-assert((encoded_image == encoded_diy).all())
-plt.imshow(img,  interpolation='none')
-plt.show()
+def add_image_to_cbddlp(buf_orig: np.ndarray, image: np.ndarray, exposure_time_s: float = 7*60):
+    image_encoded = encode_image(image.T)
+    parsed_orig = parse_cbddlp(buf_orig)
+    metadata_sz = buf_orig.size - parsed_orig['image_len'][0]
+    print(f"adding image of size {image_encoded.size} to orig size {parsed_orig['image_len']}")
+    assert(parsed_orig['image_offset'] + parsed_orig['image_len'] == buf_orig.size)
+    assert(metadata_sz > 0)
+    out_sz = metadata_sz + image_encoded.size
 
+    buf_out = np.zeros(out_sz, np.uint8)
+    buf_out[0:metadata_sz] = buf_orig[0:metadata_sz]
+
+    parsed_out = parse_cbddlp(buf_out)
+    # assumption: there is only one layer in the original image
+    buf_out[int(parsed_out['image_offset']):] = image_encoded
+    # [:] syntax modifies in-place rather than making that variable an int
+    parsed_out['image_len'][:] = image_encoded.size
+    parsed_out['exposure_time_s'][:] = exposure_time_s
+    parsed_out['z_height'][:] = 0  # I hope you took your print head off.
+    return buf_out
+
+
+def parse_gerber_svg(fname: str, dpi: int):
+    with open(fname, 'rb') as f:
+        buf = f.read()
+    png = cairosvg.svg2png(bytestring=buf, dpi=dpi)
+    image = imageio.imread(png)
+    image = image.sum(axis=-1)  # get rid of rgba channel
+    image[image > 0] = 1
+    image = image.astype(np.uint8)
+    return image
+
+def read_buf(fname: str):
+    with open(fname, 'rb') as f:
+        buf = f.read()
+    buf = np.frombuffer(buf, dtype=np.uint8)
+    return buf
+
+if __name__ == "__main__":
+    import os; os.chdir(r"C:\Users\harry\Documents\Git\Resin-printer-pcb")  # jank to run in vscode
+    base_fname = r"single_layer_circle.cbddlp"
+    gerber_fname = r"C:\Users\harry\Documents\Git\Resin-printer-pcb\fet_1-F_Cu.svg"
+
+    buf = read_buf(base_fname).copy()
+    parsed = parse_cbddlp(buf)
+    screen_res = parsed['screen_res']; printer_dims = parsed['printer_dims']
+
+    dpi = 25.4 * screen_res / printer_dims[0:2]
+    assert(np.abs(1 - dpi[0] / dpi[1]) < 0.001)  # x and y dpi should be the same
+    dpi = int(round(dpi[0]))
+
+    gerber = parse_gerber_svg(gerber_fname, dpi)
+    # gerber[:] = 1
+    if gerber.shape[0] > screen_res[0]:
+        gerber = gerber.T
+    assert((gerber.shape <= screen_res).all()), "gerber is too big to print"
+    out_image = np.zeros(screen_res, dtype=np.uint8)
+    out_image[0:gerber.shape[0], 0:gerber.shape[1]] = gerber
+    out_image = np.flipud(out_image)
+    out_image = 1 - out_image
+
+    buf_out = add_image_to_cbddlp(buf, out_image)
+    out_fname = "out.cbddlp"
+    with open(out_fname, "wb") as f:
+        f.write(buf_out)
+    try:
+        with open(os.path.join(r"G:\ "[0:-1], out_fname), "wb") as f:
+            f.write(buf_out)
+    except FileNotFoundError: 
+        print("could not write to usb")
+
+    if False:
+        encoded_image = parsed['encoded_image']
+        img = decode_image(encoded_image, screen_res)
+        encoded_diy = encode_image(img)
+        assert((encoded_image == encoded_diy).all())
+        plt.figure(); plt.title("contents of base file")
+        plt.imshow(img,  interpolation='none')
+        plt.figure(); plt.title("the gerber file to insert")
+        plt.imshow(gerber,  interpolation='none')
+        plt.figure(); plt.title("the output file with the gerber file inserted")
+
+        output_reloaded = read_buf(out_fname).copy()
+        img2 = decode_image(parse_cbddlp(output_reloaded)['encoded_image'], screen_res)
+        plt.imshow(img2,  interpolation='none')
+        plt.show(); 
